2c3b38dc956d6a8c9e7c3d97faf8a6617562f265
Sveta <81550686+chtozaserikova@users.noreply.github.com>
2022-05-09 10:33:46 +0300

    Update preprocessing_rec_systems.py

--- preprocessing_rec_systems.py
+++ preprocessing_rec_systems.py
@@ -15,5 +15,12 @@ def clean_data(df):
     df['genres'] = df['genres'].fillna('[]')
     df['genres'] = df['genres'].apply(literal_eval)
     df['genres'] = df['genres'].apply(lambda x: [i['name'].lower() for i in x] if isinstance(x, list) else [])
+    
+    C = df['vote_average'].mean()
+    m = df['vote_count'].quantile(0.8)
+    df = df.copy().loc[df['vote_count'] >= m]
+    # IMDB формула
+    df['score'] = df.apply(lambda x: (x['vote_count']/(x['vote_count']+m) * x['vote_average']) 
+                                        + (m/(m+x['vote_count']) * C), axis=1)
 
     return df
